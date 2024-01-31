ae20927c3763006996f86e94d941cb11a31f0cd9
Evann <evann.jacquot@artfx.fr>
2024-01-31 15:11:28 +0100

    [add] Earn Money function

--- BasicCPP/BasicCPP.cpp
+++ BasicCPP/BasicCPP.cpp
@@ -2,11 +2,23 @@
 
 using namespace std;
 
+void EarnMoney(int* wallet, int wage) {
+    *wallet += wage;
+    *wallet -= wage / 4;
+}
+
 int main()
 {
-    cout << "Hello World!\n";
     string answer;
+    int money = 1000;
+    int* bankAccount = &money;
+    cout << "Hello World!\n";
     cin >> answer;
     cout << "Hello " << answer << " you are stored in " << &answer <<"\n";
+    cout << "You can find my money at this adress : " << bankAccount << "\n";
+
+    EarnMoney(bankAccount, 100000);
+
+    cout << "" << money;
 }
 
